b9e612b668eecf5200fae3f353176c61fd9ff5b0
VisarGjema1 <visar.gjema@beriflapp.com>
2024-12-18 11:57:50 +0100

    feature/createHomePage - Created about, faq, footer, header, hero, and products components - Added styles to the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../app/header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    ProductsComponent,
+    ContactComponent,
+    FaqComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'webApplication';
